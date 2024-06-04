8c42e4d72611f8c08003ae84aa7d1d2b3a2ea098
Cristhian Benitez <cristhiantilleria@gmail.com>
2024-06-04 17:51:13 -0400

    finished random quote project

--- random-quote/index.js
+++ random-quote/index.js
@@ -4,22 +4,35 @@ const tagsElement = document.getElementById('tags');
 const newQuoteBtn = document.getElementById('new-quote');
 const copyQuoteBtn = document.getElementById('copy-quote');
 
-const API = 'https://api.quotable.io/random';
+const API = 'https://api.api-ninjas.com/v1/quotes';
 
-async function getQuote() {
-  const response = await fetch(API);
-  const data = await response.json();
-  console.log(data);
+const API_KEY = 'dP++w4B++e0bdAMTTatTow==GvcYjdeWcZIoAHWs';
 
-  quoteElement.innerText = data.content;
-  authorElement.innerText = data.author;
-
-  data.tags.forEach((tag) => {
+const addTags = (tags) => {
+  while (tagsElement.firstChild) {
+    tagsElement.removeChild(tagsElement.firstChild);
+  }
+  tags.forEach((tag) => {
     const tagElement = document.createElement('span');
     tagElement.classList.add('tag');
     tagElement.innerText = tag;
     tagsElement.appendChild(tagElement);
   });
+};
+
+async function getQuote() {
+  const response = await fetch(API, {
+    headers: {
+      'X-Api-Key': API_KEY
+    }
+  });
+  const data = await response.json();
+  console.log(data);
+
+  quoteElement.innerText = `“${data[0].quote}”`;
+  authorElement.innerText = data[0].author;
+
+  addTags([data[0].category]);
 }
 
 getQuote();
